d72ff31c09fbcf18457c21467dc14ff2fee4c7d1
Junjie <git@junjie.im>
2021-10-27 16:16:23 +0800

    [change] `hjkl` mode to `ijkl` mode

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -6,17 +6,30 @@
   :config
   (evil-mode 1))
 
+(define-key evil-normal-state-map "i" 'evil-previous-line)
+(define-key evil-normal-state-map "j" 'evil-backward-char)
+(define-key evil-normal-state-map "k" 'evil-next-line)
+(define-key evil-normal-state-map "h" 'evil-insert)
+
+(defun my-hjkl-retation (_mode mode-keymaps &rest _rest)
+  (evil-collection-translate-key 'normal mode-keymaps
+    "k" "j"
+    "i" "k"
+    "j" "h"
+    "h" "i"))
+
+(use-package evil-collection
+  :after evil
+  :config
+  (add-hook 'evil-collection-setup-hook #'my-hjkl-retation)
+  (evil-collection-init))
+
 (use-package evil-nerd-commenter
   :ensure t)
 
 (use-package evil-surround
   :config
   (global-evil-surround-mode 1))
-
-(use-package evil-collection
-  :after evil
-  :config
-  (evil-collection-init))
 
 (provide 'init-evil)
 ;;; init-evil.el ends here
