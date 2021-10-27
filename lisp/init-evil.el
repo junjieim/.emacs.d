d29e36a1762779a801a749546bdf3c8da1853ad0
Junjie <git@junjie.im>
2021-10-27 20:35:30 +0800

    [new] "SPC" leader key: `ff` for find-file, `g` for magit, `j` for avy-goto-char-timer

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -5,39 +5,29 @@
   (setq evil-want-keybinding nil)
   :config
   (setq evil-move-cursor-back nil) ;; move the cursor back when exiting insert mode 
-  (evil-mode 1))
+  ;; normal state keymaps
+  (define-key evil-normal-state-map "gh" 'evil-beginning-of-line)
+  (define-key evil-normal-state-map "gl" 'evil-end-of-line)
+  (evil-mode 1)
+  )
 
-;; normal state keymaps
-(define-key evil-normal-state-map "i" 'evil-previous-line)
-(define-key evil-normal-state-map "j" 'evil-backward-char)
-(define-key evil-normal-state-map "k" 'evil-next-line)
-(define-key evil-normal-state-map "h" 'evil-insert)
-(define-key evil-normal-state-map "gj" 'evil-beginning-of-line)
-(define-key evil-normal-state-map "gl" 'evil-end-of-line)
-
-;; visual state keymaps
-(define-key evil-visual-state-map "i" 'evil-previous-line)
-(define-key evil-visual-state-map "j" 'evil-backward-char)
-(define-key evil-visual-state-map "k" 'evil-next-line)
-(define-key evil-visual-state-map "h" 'evil-insert)
-
-(defun my-hjkl-retation (_mode mode-keymaps &rest _rest)
-  (evil-collection-translate-key 'normal mode-keymaps
-    "k" "j"
-    "i" "k"
-    "j" "h"
-    "h" "i"))
+(evil-set-leader '(normal visual) (kbd "SPC"))
+(evil-define-key '(normal visual) 'global
+  (kbd "<leader>ff") 'counsel-find-file)
+(evil-define-key '(normal visual) 'global
+  (kbd "<leader>g") 'magit)
+(evil-define-key '(normal visual) 'global
+  (kbd "<leader>j") 'evil-avy-goto-char-timer)
 
 (use-package evil-collection
   :after evil
   :config
-  (add-hook 'evil-collection-setup-hook #'my-hjkl-retation)
   (evil-collection-init))
 
 (use-package evil-escape
   :config
   (setq-default evil-escape-delay 0.2)
-  (setq-default evil-escape-key-sequence "jl")
+  (setq-default evil-escape-key-sequence "jk")
   (evil-escape-mode 1)
   )
 
