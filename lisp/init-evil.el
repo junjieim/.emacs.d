414525e82806197d4a45764a37b467458444bdf8
Junjie <git@junjie.im>
2021-10-29 14:11:08 +0800

    [new] keybinding with leader key within evil about file, buffer, window, avy command.

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -6,18 +6,29 @@
   :config
   (setq evil-move-cursor-back nil) ;; move the cursor back when exiting insert mode 
   ;; normal state keymaps
-  (define-key evil-normal-state-map "gh" 'evil-beginning-of-line)
-  (define-key evil-normal-state-map "gl" 'evil-end-of-line)
+  ;; (define-key evil-normal-state-map "gh" 'evil-beginning-of-line)
+  ;; (define-key evil-normal-state-map "gl" 'evil-end-of-line)
+  (evil-set-leader '(normal visual) (kbd "SPC"))
+  (evil-define-key '(normal visual) 'global
+    "gh" 'evil-beginning-of-line
+    "gl" 'evil-end-of-line
+    ;; file
+    (kbd "<leader>ff") 'counsel-find-file
+    ;; git
+    (kbd "<leader>g") 'magit
+    ;; avy
+    (kbd "<leader>j") 'evil-avy-goto-char-timer
+    ;; buffer
+    (kbd "<leader>bs") 'ivy-switch-buffer
+    (kbd "<leader>bj") 'previous-buffer
+    (kbd "<leader>bk") 'next-buffer
+    ;; window
+    (kbd "<leader>w1") 'delete-other-windows
+    (kbd "<leader>w2") 'split-window-below
+    (kbd "<leader>w3") 'split-window-right
+    )
   (evil-mode 1)
   )
-
-(evil-set-leader '(normal visual) (kbd "SPC"))
-(evil-define-key '(normal visual) 'global
-  (kbd "<leader>ff") 'counsel-find-file)
-(evil-define-key '(normal visual) 'global
-  (kbd "<leader>g") 'magit)
-(evil-define-key '(normal visual) 'global
-  (kbd "<leader>j") 'evil-avy-goto-char-timer)
 
 (use-package evil-collection
   :after evil
